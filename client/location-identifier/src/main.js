a54a58fd356a7cb071e1def8c5cee4b23f915633
tqbdev <tranquocbao3897@gmail.com>
2018-11-06 22:21:56 +0700

    [FEATURE] - Add all login function

--- client/location-identifier/src/main.js
+++ client/location-identifier/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from '@/router/router'
+import Vuetify from 'vuetify'
+import {sync} from 'vuex-router-sync'
+import 'vuetify/dist/vuetify.min.css'
+import store from '@/store/store'
+
+import Panel from '@/components/globals/Panel'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+Vue.component('panel', Panel)
+
+sync(store, router)
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store,
+  router
 }).$mount('#app')
