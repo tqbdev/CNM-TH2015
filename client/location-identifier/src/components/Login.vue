aa4954ed142f3cce6ac6696ab97b27b1a030a5d4
tqbdev <tranquocbao3897@gmail.com>
2018-11-06 22:23:25 +0700

    [FEATURE] - Done all login page

---
+++ client/location-identifier/src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <panel title="Login">
+        <form
+          name="request-receiver-login-form"
+          autocomplete="off">
+          <v-text-field
+            label="Username"
+            v-model="username"
+          ></v-text-field>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+          ></v-text-field>
+        </form>
+        <div class="danger-alert" v-html="error"></div>
+        <v-btn
+          dark
+          class="cyan"
+          @click="login">
+          Login
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import AuthencationService from '@/services/AuthencationService'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      username: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const response = await AuthencationService.login({
+          username: this.username,
+          password: this.password
+        })
+
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({
+          name: 'requestList'
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
